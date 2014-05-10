e532b94f34778fff3aacc2352f26a5162e8c969f
BlackSEA- <blacksea8393@gmail.com>
2014-05-10 19:40:25 +0300

    teamviewer:version bump to 9 series

--- teamviewer/actions.py
+++ teamviewer/actions.py
@@ -1,22 +1,35 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Licensed under the GNU General Public License, version 2.
-# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+# Licensed under the GNU General Public License, version 3.
+# See the file http://www.gnu.org/licenses/gpl.txt
 
-from pisi.actionsapi import get
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import shelltools
+from pisi.actionsapi import get
 
 WorkDir = "."
-
-NoStrip = ["/opt/teamviewer/teamviewer/7/wine/drive_c/Program Files/TeamViewer/Version7/tvwine.dll.so"]
+NoStrip = ["/opt/teamviewer9/tv_bin/wine/drive_c/TeamViewer/tvwine.dll.so"]
 
 def setup():
     shelltools.system("rpm2targz -v %s/teamviewer_linux.rpm" %get.workDIR())
-    shelltools.system("tar xfvz %s/teamviewer_linux.tar.gz --exclude=usr" %get.workDIR())
-    shelltools.chmod("%s/opt/teamviewer/teamviewer/7/bin/*" %get.workDIR())
-
+    shelltools.system("tar xfvz %s/teamviewer_linux.tar.gz --exclude=var --exclude=etc --exclude=usr" %get.workDIR())
+    
 def install():
     pisitools.insinto("/opt/", "./opt/*")
-    pisitools.dosym("/opt/teamviewer/teamviewer/7/bin/teamviewer", "/usr/bin/teamviewer")
+    pisitools.remove("/opt/teamviewer9/tv_bin/xdg-utils/xdg-email")
+    
+    #necessary symlinks
+    pisitools.dosym("/opt/teamviewer9/tv_bin/teamviewerd", "etc/init.d/teamviewerd")
+    pisitools.dosym("/opt/teamviewer9/tv_bin/script/teamviewer", "usr/bin/teamviewer")
+    pisitools.dosym("/opt/teamviewer9/logfiles", "var/log/teamviewer")
+    pisitools.dosym("/opt/teamviewer9/config", "etc/teamviewer")
+    
+    pisitools.dodoc("%s/opt/teamviewer9/doc/License.txt" %get.workDIR())
+    
+    
+    shelltools.chmod("%s/opt/teamviewer9/*" % get.installDIR(),0755)  
+    
+    
+    
+    
